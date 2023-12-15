49c7232d658a1d24337575535f05f9174ac5921d
GS 16 <gs16@vtechys.com>
2023-12-15 10:35:30 -0800

    First commit

---
+++ src/test/java/com/AllCode/OtherSites/BrowserSelection.java
@@ -0,0 +1,61 @@
+package com.AllCode.OtherSites;
+
+
+	
+
+	import java.awt.Desktop;
+	import java.io.IOException;
+	import java.net.URI;
+
+	public class BrowserSelection {
+		public  void browserSelectiontoOpenUrl( String browser,String link) {
+			try {
+				  String[] command = {"cmd", "/c", "start", browser, link};
+
+		          // Execute the command
+		          Process process = new ProcessBuilder(command).start();
+
+		          // Optionally, you can wait for the process to finish
+		          int exitCode = process.waitFor();
+		          System.out.println(browser+" browser opened with exit code: " + exitCode);
+			} catch (IOException | InterruptedException e) {
+		        e.printStackTrace();
+		    }
+		}
+		public static void browserValue(String browser) {
+			
+			       // getDataFromExcelSheet("Sheet1");
+				 try {
+			            if (browser.equalsIgnoreCase("chrome")) {
+			                openBrowserWithPowerShell("src\\test\\resources\\Chrome.ps1");
+			            } else if (browser.equalsIgnoreCase("edge")) {
+			                openBrowserWithPowerShell("src\\test\\resources\\Edge.ps1");
+			            } else if (browser.equalsIgnoreCase("firefox")) {
+			                openBrowserWithPowerShell("src\\test\\resources\\Firefox.ps1");
+			            } else {
+			                openDefaultBrowser("https://www.google.com/");
+			            }
+			        } catch (Exception e) {
+			            // Opening the specified browser failed, open the default browser
+			            openDefaultBrowser("https://www.google.com/");
+			        }
+			    }
+
+			    private static void openBrowserWithPowerShell(String scriptPath) throws Exception {
+			        String command = "powershell.exe -ExecutionPolicy Bypass -File \"" + scriptPath + "\"";
+			        Process process = Runtime.getRuntime().exec(command);
+			        process.waitFor();
+			    }
+
+			    private static void openDefaultBrowser(String url) {
+			        try {
+			            URI uri = new URI(url);
+			            Desktop.getDesktop().browse(uri);
+			        } catch (Exception e) {
+			            e.printStackTrace();
+			        }
+			    }}
+			      
+
+	
+
